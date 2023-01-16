1c05c7a53e63cdf8cf16b618b11296f7e66d8d3e
furanzKafka <furkankizilkoca@gmail.com>
2023-01-16 23:39:05 +0300

    Add project files. and initial migration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ActorMovie>().HasKey(x => new
+            {
+                x.MovieId,
+                x.ActorId
+            });
+
+            modelBuilder.Entity<ActorMovie>().HasOne(x => x.Movie).WithMany(y => y.ActorMovies).HasForeignKey(x => x.MovieId);
+            modelBuilder.Entity<ActorMovie>().HasOne(x => x.Actor).WithMany(y => y.ActorMovies).HasForeignKey(x => x.ActorId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<ActorMovie> ActorMovies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+    }
+}
