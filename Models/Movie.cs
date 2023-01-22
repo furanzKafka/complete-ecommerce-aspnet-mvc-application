4610b64c52c5dac103b859097d512f6080f6caee
furanzKafka <furkankizilkoca@gmail.com>
2023-01-22 14:31:28 +0300

    seed data

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -10,7 +10,7 @@ namespace eTickets.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Price { get; set; }
+        public double Price { get; set; }
         public string ImageURL { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
