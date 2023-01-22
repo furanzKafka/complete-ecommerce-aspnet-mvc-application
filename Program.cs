a3c6f95839434e0ddec3741aea3115a2b0db83a6
furanzKafka <furkankizilkoca@gmail.com>
2023-01-22 22:47:24 +0300

    setting navbar but drodown not work

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Movie}/{action=Index}/{id?}");
 
 //seed data
 //search difference between         builder.Services.AddScoped<DbInitializer>();
