9e0a75cbca81b1fc4984afbba08b40be9ed32ece
furanzKafka <furkankizilkoca@gmail.com>
2023-01-25 02:40:08 +0300

    add movie create

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -1,5 +1,8 @@
 ﻿using eTickets.Data;
+using eTickets.Data.Services;
+using eTickets.Data.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Immutable;
 
@@ -7,17 +10,76 @@ namespace eTickets.Controllers
 {
     public class MovieController : Controller
     {
-        private readonly AppDbContext _context;
+        private readonly IMovieService _service;
 
-        public MovieController(AppDbContext context)
+        public MovieController(IMovieService service)
         {
-            _context = context;
+            _service = service;
         }
 
         public async Task<IActionResult> Index()
         {
-            var allMovie =await _context.Movies.Include(x=>x.Cinema).ToListAsync();
+            var allMovie =await _service.GetAllAsync(x=>x.Cinema);
             return View(allMovie);
+        }
+        //[HttpGet]
+        //public IActionResult Create()
+        //{
+        //    ViewData["Welcome"] = "weqewq";
+        //    ViewBag.Description = "hiiii";
+        //    return View();
+        //}
+        public async Task<IActionResult> Create()
+        {
+            var movieDropdownsData = await _service.GetNewMovieDropdownsValues();
+
+            ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id", "Name");
+            ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+            ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NewMovieVM movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                var movieDropdownsData = await _service.GetNewMovieDropdownsValues();
+
+                ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id", "Name");
+                ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+                ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
+
+                return View(movie);
+            }
+
+            await _service.AddNewMovieAsync(movie);
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var result = await _service.GetMovieByIdAsync(id);
+
+            if (result == null) return View("Empty");
+            return View(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var result = await _service.GetByIdAsync(id);
+
+            if (result == null) return View("NotFound");
+            return View(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _service.GetByIdAsync(id);
+
+            if (result == null) return View("NotFound");
+            return View(result);
         }
     }
 }
--- Data/Services/IMovieService.cs
+++ Data/Services/IMovieService.cs
@@ -1,6 +1,14 @@
-﻿namespace eTickets.Data.Services
+﻿using eTickets.Data.Base;
+using eTickets.Data.ViewModels;
+using eTickets.Models;
+
+namespace eTickets.Data.Services
 {
-    public interface IMovieService
+    public interface IMovieService : IEntityBaseRepository<Movie>
     {
+        Task<Movie> GetMovieByIdAsync(int id);
+        Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues();
+        Task AddNewMovieAsync(NewMovieVM data);
+        Task UpdateMovieAsync(NewMovieVM data);
     }
 }
